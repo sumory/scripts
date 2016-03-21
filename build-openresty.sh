8263a7e69d5699b8d2a5010e5bbf6a0a15f31d2c
sumory <sumory.wu@gmail.com>
2016-03-21 16:08:05 +0800

    update download url

--- build-openresty.sh
+++ build-openresty.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 base_dir=$(pwd)
-openresty_tar="ngx_openresty-1.9.7.3.tar.gz"
+openresty_tar="openresty-1.9.7.3.tar.gz"
 pcre_tar="pcre-8.33.tar.gz"
 
 echo "$(tput setaf 2)Downloading openresty...$(tput sgr0)"
-curl -O -L "http://openresty.org/download/$openresty_tar"
+curl -O -L "https://openresty.org/download/$openresty_tar"
 echo "$(tput setaf 2)Downloading pcre...$(tput sgr0)"
 curl -O -L "http://downloads.sourceforge.net/sourceforge/pcre/$pcre_tar"
 
@@ -20,7 +20,7 @@ pcre_dir="$base_dir/$(ls -d pcre*/ | head -n 1)"
 echo "$(tput setaf 2)Building openresty...$(tput sgr0)"
 (
 	cd $(ls -d ngx_openresty*/ | head -n 1)
-	./configure --with-pcre=$pcre_dir --with-http_postgres_module --with-pcre-jit
+	./configure --with-pcre=$pcre_dir --with-http_postgres_module --with-pcre-jit  --with-http_stub_status_module
 	make
 	mkdir /tmp/openresty
 	make DESTDIR=/tmp/openresty install
