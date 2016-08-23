5fd76677e6d4f76b478a028bd5ffae4b3691ab2e
sumory <sumory.wu@gmail.com>
2016-08-23 17:36:28 +0800

    add install script for OpenResty on MacOSX

---
+++ install_openresty_macosx.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+OPENRESTY_VERSION="1.9.15.1"
+OPENRESTY_DIR=$1
+
+
+if [ -n "$OPENRESTY_DIR" ];then
+    OPENRESTY_DIR="${OPENRESTY_DIR}" #add sub folder for lor
+   echo "use defined install path: "${OPENRESTY_DIR}
+else
+   OPENRESTY_DIR="/opt/openresty"
+   echo "use default install path: ${OPENRESTY_DIR}"
+fi
+
+
+wget https://openresty.org/download/openresty-$OPENRESTY_VERSION.tar.gz
+tar zxvf openresty-$OPENRESTY_VERSION.tar.gz
+cd openresty-$OPENRESTY_VERSION
+
+./configure \
+            --with-cc-opt="-I/usr/local/Cellar/pcre/8.38/include -I/usr/local/opt/openssl/include" \
+            --with-ld-opt="-L/usr/local/Cellar/pcre/8.38/lib -L/usr/local/opt/openssl/lib"  \
+            -j8 \
+            --with-http_stub_status_module \
+            --with-luajit \
+            --prefix="$OPENRESTY_DIR"
+make
+make install
+
+${OPENRESTY_DIR}/nginx/sbin/nginx -v
+echo "OpenResty installed."
